bd14b3445a101cc5d07222f13a301f510ea070b6
429272-14 <429272-14>
2019-12-23 10:26:35 +0300

    core

---
+++ core/EquationInterface.java
@@ -0,0 +1,13 @@
+package core;
+        import java.util.ArrayList;
+
+public interface EquationInterface {
+    /**
+     * * @param double a
+     * * @param double b
+     * * @param double c
+     * * * @return array */
+    ArrayList<Double> equation(double a, double b, double c);
+
+
+}
---
+++ core/LogInterface.java
@@ -0,0 +1,5 @@
+package core;
+public interface LogInterface {
+    String  log(String str);
+    void write();
+}
